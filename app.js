53c92ccd8391d6e603c62111518a6caed9a3f1fa
Jonas Surmann <jonassurmann@Jonass-MacBook-Pro.local>
2021-02-10 08:40:33 +0800

    test set-up

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
+console.log('test');
+  // Create Dino Constructor
+    function Dino() {
+      this.species = species;
+      this.weight = weight;
+      this.height = height;
+      this.diet = diet;
+      this.where = where;
+      this.when = when;
+      this.fact = fact;
+    }
 
-    // Create Dino Constructor
+    const dinoSpecies = new Dino();
 
+    console.log('test');
 
     // Create Dino Objects
-
-
+    let Triceratops = {
+      species : dinoSpecies.species,
+      weight : dinoSpecies.weight,
+      height : dinoSpecies.height,
+      diet : dinoSpecies.diet,
+      where : dinoSpecies.where,
+      when : dinoSpecies.when,
+      fact : dinoSpecies.fact
+    };
     // Create Human Object
+    let Human = {
+      species : dinoSpecies.species,
+      weight : dinoSpecies.weight,
+      height : dinoSpecies.height,
+      diet : dinoSpecies.diet,
+      where : dinoSpecies.where,
+      when : dinoSpecies.when,
+      fact : dinoSpecies.fact
+    };
 
     // Use IIFE to get human data from form
 
-
+    const newCard = document.createElement('div')
+      const cardTitle = document.createElement('h3')
+      cardTitle.innerHTML = generateData.species
+      cardTitle.innerHTML +=
+                `<img src="./images/${generateData.species}.png" alt="${generateData.species} image"/>`
+      const cardFact = document.createElement('p')
+      cardFact.innerHTML = generateData.fact
+      newCard.classList.add('grid-item')
+      grid.appendChild(newCard)
+      newCard.appendChild(cardTitle)
+      newCard.appendChild(cardFact)
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
