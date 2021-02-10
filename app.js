6695d1689e74422982e7f94948259c7a0845e6a6
Jonas Surmann <jonassurmann@Jonass-MacBook-Pro.local>
2021-02-11 07:14:36 +0800

    Test to set-up local server

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-  let Dino1 = {
-        species: 'Triceratops',
-        weight: 13000,
-        height: 114,
-        diet: 'herbavor',
-        where: 'North America',
-        when: 'Late Cretaceous',
-        fact: 'First discovered in 1889 by Othniel Charles Marsh'
-    };
-    let Dino2 = {
-      species: 'Triceratops',
-      weight: 13000,
-      height: 114,
-      diet: 'herbavor',
-      where: 'North America',
-      when: 'Late Cretaceous',
-      fact: 'First discovered in 1889 by Othniel Charles Marsh'
-    };
-    let Dino3 = {
-      species: 'Triceratops',
-      weight: 13000,
-      height: 114,
-      diet: 'herbavor',
-      where: 'North America',
-      when: 'Late Cretaceous',
-      fact: 'First discovered in 1889 by Othniel Charles Marsh'
-    };
-
-    const dinoArray = [Dino1, Dino2, Dino3];
-
-    console.log(dinoArray[1].species);
-
+  (async() => {
 
     // Create Dino Constructor
     function DinoConstructor(species, weight, height, diet, where, when, fact, image) {
@@ -43,42 +12,24 @@
       this.image = image;
     };
 
-
-    //const dinoClass = new DinoConstructor(species, weight, height, diet, where, when, fact, image);
-    // Create Dino Objects
-
-    //Testing the access of local server
-    //console.log('Fetching ' + dinosJSON());
-    const dinosJSON = async () => {
+    const dinosData = async () => {
       const response = await fetch("./dino.json");
       const data = await response.json();
       return data.Dinos;
     };
-    const accessDino = async () => {
-      const a = await dinosJSON;
-      //console.log(a);
-    };
-    //accessDino();
-    //console.log('Fetching ' + accessDino());
 
+    btn.addEventListener('click', async function(event) {
+      const dinos = await dinosData();
+      console.log('Fetching ', await dinosData());
+    });
 
+  })();
 
-
+      // Create Dino Objects
 
     // Create Human Object
 
     // Use IIFE to get human data from form
-
-
-
-    // the listener callback is async because
-    // we need to wait for dinosData to complete
-      btn.addEventListener('click', async function(event) {
-        // wait dinosData complete
-      const dinos = await dinosData();
-      // do something with dinos
-      });
-
 
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches.
